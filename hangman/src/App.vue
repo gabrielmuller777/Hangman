6aeecc3e9c2f93a76fd675c2aa4ace00f8ec5b86
FernandoItami <fernandoitami@gmail.com>
2020-03-29 02:10:12 +0900

    first commit

---
+++ hangman/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <label class="title">HANGMAN</label>
+    <Hangman/>
+  </div>
+</template>
+
+<script>
+import Hangman from './components/Hangman.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Hangman
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+}
+html{
+  width: 100%;
+  background-color: black;
+}
+body{
+  width: 70%;
+  background-color: rgb(70, 70, 70);
+  margin: 20px auto 0 auto;
+}
+.title {
+  font-size: 50px;
+}
+</style>
